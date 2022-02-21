971b887df5be58f83db3246da3081a6c3964d1fd
Fathyy <fathiabdi254@gmail.com>
2022-02-21 05:30:48 -0800

    Arrays

---
+++ arrays.php
@@ -0,0 +1,54 @@
+<?php 
+$subject = array("Kisw", "eng", "Bio", "maths", "Chem");
+
+// retrieving data
+echo $subject[0];
+echo $subject[1];
+echo $subject[2];
+echo $subject[3];
+echo $subject[4];
+
+echo "<br>";
+
+$colors = array("red", " orange", "yellow", "green", "blue", "indigo", "violiet");
+
+echo $colors[0];
+echo $colors[1];
+echo $colors[2];
+echo $colors[3];
+echo $colors[4];
+echo $colors[5];
+echo $colors[6];
+
+
+echo "<br>";
+
+foreach($subject as $individualSubject){
+    echo "subject $individualSubject <br>";
+}
+
+echo "<br>";
+
+
+?>
+
+<h2>Colors of a rainbow</h2>
+
+<ul style="list-style-type: disc;">
+<?php
+foreach($colors as $key => $color){
+    echo "<li> $key is $color </li>";
+}
+?>
+
+</ul>
+
+<?php 
+echo "<br>";
+
+$friends = array("Mary", "john", "Nasra", "Ayall", "Lisa");
+foreach($friends as $key => $friend) {
+    $val = $key+1;
+    echo "$val is $friend <br>";
+}
+?>
---
+++ associative.php
@@ -0,0 +1,4 @@
+<?php
+$score = ["html" =>8, "PHP"=>9, "SQL"=>8];
+echo $score["SQL"];
+?>
